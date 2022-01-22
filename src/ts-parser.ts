4afdf2c2597ec389e750f97bb79b8ccd63da8b78
Olivier Huin <olih@users.noreply.github.com>
2022-01-22 19:56:34 +0000

    Support imports using ts-morph

--- src/ts-parser.ts
+++ src/ts-parser.ts
@@ -1,6 +1,44 @@
-import { ParseInfo } from './model.js';
+import { Project, ImportDeclaration } from 'ts-morph';
+import { ImportInfo, ParseInfo } from './model.js';
 
-export const parseTsContent = (_source: string, _content: string): ParseInfo => ({
-  imports: [],
-  functions: [],
-});
+const extractImportInfo =
+  (source: string) =>
+  (importDecl: ImportDeclaration): ImportInfo[] => {
+    const namedImports =
+      importDecl
+        .getImportClause()
+        ?.getNamedImports()
+        .map((ni) => ni.getText()) || [];
+    const from = importDecl.getModuleSpecifierValue();
+    const defaultImport = importDecl.getDefaultImport();
+    if (defaultImport !== undefined) {
+      return [
+        {
+          identifier: defaultImport.getText(),
+          source,
+          from,
+        },
+      ];
+    }
+    return namedImports.map((ni) => ({
+      identifier: ni,
+      source,
+      from,
+    }));
+  };
+
+export const parseTsContent = (source: string, content: string): ParseInfo => {
+  const project = new Project();
+  project.createSourceFile(source, content);
+  const current = project.getSourceFile(source);
+  const currentImports = current?.getImportDeclarations() || [];
+
+const parsed = {
+    imports: currentImports.flatMap(extractImportInfo(source)),
+    functions: [],
+  }
+
+  console.log(parsed)
+
+  return parsed;
+};
--- test/ts-parser.test.ts
+++ test/ts-parser.test.ts
@@ -2,7 +2,7 @@ import { parseTsContent } from '../src/ts-parser';
 
 const someImports = `
 import { Command } from 'commander';
-import fs from 'fs';
+import nodeFs from 'fs';
 import { commanding } from './index.js';
 import {
     GenerateTypedocAction,
@@ -16,11 +16,52 @@ import {
 
 describe('ts-parser', () => {
   it('parse imports', () => {
-    const actual = parseTsContent('./source.js',someImports);
+    const actual = parseTsContent('./source.ts', someImports);
     expect(actual).toMatchInlineSnapshot(`
       Object {
         "functions": Array [],
-        "imports": Array [],
+        "imports": Array [
+          Object {
+            "from": "commander",
+            "identifier": "Command",
+            "source": "./source.ts",
+          },
+          Object {
+            "from": "fs",
+            "identifier": "nodeFs",
+            "source": "./source.ts",
+          },
+          Object {
+            "from": "./index.js",
+            "identifier": "commanding",
+            "source": "./source.ts",
+          },
+          Object {
+            "from": "./model.js",
+            "identifier": "GenerateTypedocAction",
+            "source": "./source.ts",
+          },
+          Object {
+            "from": "./model.js",
+            "identifier": "GenerateTypedocActionOpts",
+            "source": "./source.ts",
+          },
+          Object {
+            "from": "./model.js",
+            "identifier": "ParseAction",
+            "source": "./source.ts",
+          },
+          Object {
+            "from": "./model.js",
+            "identifier": "ParseActionOpts",
+            "source": "./source.ts",
+          },
+          Object {
+            "from": "./model.js",
+            "identifier": "RunnerContext",
+            "source": "./source.ts",
+          },
+        ],
       }
     `);
   });
